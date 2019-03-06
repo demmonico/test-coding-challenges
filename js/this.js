10c0b7b2ce20fcc649fd5a7170bacde0cfc099d5
demmonico <dep.soft@mail.ru>
2019-03-06 20:14:15 +0200

    UPD js this

--- js/this.js
+++ js/this.js
@@ -3,6 +3,8 @@ function() {
     this.name = 'test';
 }
 
+/////////////////
+// 1 simple call
 
 // test test
 function fn() {
@@ -11,6 +13,9 @@ function fn() {
 fn();
 console.log(name, this.name);
 
+
+/////////////////
+// as a method
 
 // test
 var obj = {
@@ -22,12 +27,75 @@ obj.fn();
 console.log(obj.name);
 
 
+/////////////////
+// new
+
 // test test test
 function fn() {
     this.name = 'test';
 }
 var r = new fn(); // equal to var r = new fn;
-// created new obj which became new this
-// returned just created object
+// created new obj which become new this
+// implicitly returns this
 // setup prototype
 console.log(name, this.name, r.name);
+
+// function prototype (ES5)
+function Person(nickname, email, born) {
+    // this = {}
+
+    this.nickname = nickname;
+    this.email = email;
+    this.born = born;
+
+    // not optimal. Will copied into each person object
+    //this.age = function() {
+    //    var now = new Date();
+    //    return now.getFullYear() - this.born;
+    //}
+
+    // return this;
+}
+Person.prototype.age = function() {
+    var now = new Date();
+    return now.getFullYear() - this.born;
+}
+var person = new Person('demmonico', 'demmonico@gmail.com', 1984);
+console.log(person, person.age());
+// 35
+/**
+ born: 1984
+ email: "demmonico@gmail.com"
+ nickname: "demmonico"
+ __proto__:
+ age: ƒ ()
+ constructor: ƒ Person(nickname, email, born)
+ __proto__: Object
+ */
+
+// same in ES6
+class PersonClass
+{
+    constructor(nickname, email, born) {
+        this.nickname = nickname;
+        this.email = email;
+        this.born = born;
+    }
+
+    age() {
+        var now = new Date();
+        return now.getFullYear() - this.born;
+    }
+}
+var personFromClass = new PersonClass('demmonico', 'demmonico@gmail.com', 1984);
+console.log(personFromClass, personFromClass.age());
+// 35
+/**
+ born: 1984
+ email: "demmonico@gmail.com"
+ nickname: "demmonico"
+ __proto__:
+ age: ƒ age()
+ constructor: class PersonClass
+ __proto__: Object
+ */
